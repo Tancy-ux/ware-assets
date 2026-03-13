19a737ce535af330985771786a40478f6811b474
Tanushree Chakravorty <tanushree998@gmail.com>
2026-03-13 18:37:57 +0530

    err fix

--- src/Pages/AllAssets.jsx
+++
@@ -1,9 +0,0 @@
-import AssetLibrary from "../components/AssetLibrary";
-
-export default function AllAssets() {
-  return;
-  // eslint-disable-next-line no-unreachable
-  <div>
-    <AssetLibrary />
-  </div>;
-}
